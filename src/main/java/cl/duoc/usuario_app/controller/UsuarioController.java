5ba53ed01d33eee182b1af9399e18dc21be45e8c
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2026-05-04 22:01:48 -0400

    Create UsuarioController.java

---
+++ src/main/java/cl/duoc/usuario_app/controller/UsuarioController.java
@@ -0,0 +1,55 @@
+package cl.duoc.usuario_app.controller;
+
+import cl.duoc.usuario_app.model.Usuario;
+import cl.duoc.usuario_app.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> obtenerUsuarios() {
+        List<Usuario> usuarios = usuarioService.obtenerTodos();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> obtenerPorId(@PathVariable Long id) {
+
+        Usuario usuario = usuarioService.obtenerPorId(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> crearUsuario(@Valid @RequestBody Usuario usuario) {
+
+        Usuario nuevoUsuario = usuarioService.guardarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoUsuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> actualizarUsuario(@PathVariable Long id, @Valid @RequestBody Usuario usuario) {
+
+        Usuario usuarioActualizado = usuarioService.actualizarUsuario(id, usuario);
+        return ResponseEntity.ok(usuarioActualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
+
+        usuarioService.eliminarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+}
